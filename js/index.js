5105bdf77cbf9ff6b419a67fff94f30ace1b0d53
Yurii Ruban <yurii.ruban97@gmail.com>
2024-10-08 15:27:22 +0100

    Add basic layout with styles

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 // Dummy script
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function Operation() {
+    this.matcher = new Map();
+    matcher.set('+', add);
+    matcher.set('-', substract);
+    matcher.set('*', multiply);
+    matcher.set('/', divide);
+
+    this.leftOperand = null;
+    this.rightOperand = null;
+    this.operator = null;
+}
+
+function operate(operation) {
+    const result = operation.operation();
+    return result;
+}
+
+function updateDisplay(value) {
+    const display = document.querySelector("#display");
+    display.textContent = value;
+}
+
+const mainOperation = new Operation();
+
